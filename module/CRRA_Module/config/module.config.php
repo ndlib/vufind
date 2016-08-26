31f1f84330dfa913d62565c4b4f3426d0be7dbef
Chris Hallberg <challber@villanova.edu>
2016-08-26 15:36:54 -0400

    Convert module.config.php to factory to fix cache error.

--- module/CRRA_Module/config/module.config.php
+++ module/CRRA_Module/config/module.config.php
@@ -1,43 +1,18 @@
 <?php
 namespace CRRA_Module\Module\Configuration;
 
-$config = ['vufind' => [
+$config = [
+    'vufind' => [
         'plugin_managers' => [
             'recorddriver' => [
                 'factories' => [
-                    'solrmarc' => function ($sm) {
-                        $driver = new \CRRA_Module\RecordDriver\SolrMarc(
-                            $sm->getServiceLocator()->get('VuFind\Config')->get('config'),
-                            null,
-                            $sm->getServiceLocator()->get('VuFind\Config')->get('searches')
-                        );
-                        $driver->attachILS(
-                            $sm->getServiceLocator()->get('VuFind\ILSConnection'),
-                            $sm->getServiceLocator()->get('VuFind\ILSHoldLogic'),
-                            $sm->getServiceLocator()->get('VuFind\ILSTitleHoldLogic')
-                        );
-                        return $driver;
-                    },
-					'solread' => function ($sm) {
-                        $driver = new \CRRA_Module\RecordDriver\SolrEad(
-                            $sm->getServiceLocator()->get('VuFind\Config')->get('config'),
-                            null,
-                            $sm->getServiceLocator()->get('VuFind\Config')->get('searches')
-                        );
-						return $driver;
-                    },
-                    'solrpp' => function ($sm) {
-                        $driver = new \CRRA_Module\RecordDriver\SolrPp(
-                            $sm->getServiceLocator()->get('VuFind\Config')->get('config'),
-                            null,
-                            $sm->getServiceLocator()->get('VuFind\Config')->get('searches')
-                        );
-						return $driver;
-                    },
+                    'solrmarc' => 'CRRA_Module\RecordDriver\Factory::getSolrMarc',
+                    'solread' => 'CRRA_Module\RecordDriver\Factory::getSolrEAD',
+                    'solrpp' => 'CRRA_Module\RecordDriver\Factory::getSolrPP'
                 ]
-            ],
-        ],
-    ],
+            ]
+        ]
+    ]
 ];
 
 return $config;
